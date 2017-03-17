c55102dc6c516e79974409d1c8f2d334bbac7ac1
hchiam <hchiam@gmail.com>
2017-03-16 21:46:01 -0400

    rename dictionary var for clarity'

--- dictAsFile_wrapper.py
+++ dictAsFile_wrapper.py
@@ -7,16 +7,16 @@ from dictAsFile_wrapper import * # copy this line to import its methods
 import pickle
 
 
-def writeDictToFile(dictName,fileName): # write dictionary to file
+def writeDictToFile(d,fileName): # write dictionary to file
     outputFile = open(fileName, 'wb')
-    pickle.dump(dictName, outputFile)
+    pickle.dump(d, outputFile)
     outputFile.close()
 
 def readFileToDict(fileName): # read file to dictionary
     pkl_file = open(fileName, 'rb')
-    dictName = pickle.load(pkl_file)
+    d = pickle.load(pkl_file)
     pkl_file.close()
-    return dictName
+    return d
 
 
 # this if statement is so that the following code only runs if this .py file is not being imported
