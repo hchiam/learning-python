e26dae2f966b15a1fe2d47e45a695473d54c99c8
hchiam <hchiam@gmail.com>
2017-04-05 16:17:50 -0400

    comment out unneeded functions

--- checkBST.py
+++ checkBST.py
@@ -41,30 +41,31 @@ def check_order(node):
         check_l_and_r = check_l and check_r
         return check_l_and_r and check_order(l) and check_order(r)
 
-def check_gets_smaller(node):
-    l = node.left
-    if l == None:
-        return True
-    else:
-        if l.data >= node.data:
-            return False
-        else:
-            return check_gets_smaller(l) and check_gets_bigger(l)
-
-def check_gets_bigger(node):
-    r = node.right
-    if r == None:
-        return True
-    else:
-        if r.data <= node.data:
-            return False
-        else:
-            return check_gets_bigger(r) and check_gets_smaller(r)
+# def check_gets_smaller(node):
+#     l = node.left
+#     if l == None:
+#         return True
+#     else:
+#         if l.data >= node.data:
+#             return False
+#         else:
+#             return check_gets_smaller(l) and check_gets_bigger(l)
+# 
+# def check_gets_bigger(node):
+#     r = node.right
+#     if r == None:
+#         return True
+#     else:
+#         if r.data <= node.data:
+#             return False
+#         else:
+#             return check_gets_bigger(r) and check_gets_smaller(r)
 
 @timePerfWrapper
 def check_binary_search_tree_():
     root = a
-    checks = check_gets_smaller(root) and check_gets_bigger(root) and check_order(root)
+    # checks = check_gets_smaller(root) and check_gets_bigger(root) and check_order(root)
+    checks = check_order(root)
     print '\nBinary Search Tree? ' + str(checks)
 
 
