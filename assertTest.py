a3fbbed088edf04337df8de0b41f5132a222f982
hchiam <hchiam@gmail.com>
2018-01-14 17:35:40 -0500

    update

--- assertTest.py
+++ assertTest.py
@@ -1,21 +1,18 @@
-def testEqual(a, b):
-    try:
-        assert a == b
-        print('PASSED assertion test a==b')
-    except AssertionError:
-        print('FAILED assertion test a==b')
+import unittest
+
+a = 1
+b = 24
+
+def add(a, b):
+    return a + b
+
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(a+b, add(a,b))
+    def test_not_equal(self):
+        self.assertNotEqual(a+b, add(a,b) + 100)
 
 
 # this if statement is so that the following code only runs if this .py file is not being imported
 if __name__ == '__main__':
-    
-    a = 1
-    b = 24
-    
-    print('a = ' + str(a) + ', b = ' + str(b))
-    testEqual(a, b)
-    
-    b = 1
-    
-    print('a = ' + str(a) + ', b = ' + str(b))
-    testEqual(a, b)
+    unittest.main()
