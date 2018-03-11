9638e5d292cfda0341ce1bd2c7f3e104a8916474
hchiam <hchiam@gmail.com>
2018-03-11 19:07:30 -0400

    add LL remove

--- linked_list_remove.py
+++ linked_list_remove.py
@@ -3,17 +3,14 @@ import linked_list_node as llnode
 Node = llnode.Node
 
 def remove(head,val):
-    if head == None:
-        return None
-    elif head.data == val:
+    n = head
+    if head.data == val:
         return head.next
-    else:
-        n = head
-        while n.next:
-            if n.next.data == val:
-                n.next = n.next.next
-                return head
-            n = n.next
+    while n.next:
+        if n.next.data == val:
+            n.next = n.next.next
+            return head
+        n = n.next
     return head
 
 # unit testing
@@ -23,7 +20,23 @@ class test(unittest.TestCase):
         a = Node(1)
         self.assertEqual(a.data, 1)
         a.add(2)
-        remove(a,2)
+        a = remove(a,2)
         self.assertEqual(a.data, 1)
+        self.assertNotEqual(a.data, None)
         self.assertEqual(a.next, None)
-unittest.main()
+        a = remove(a,1)
+        self.assertEqual(a, None)
+        a = Node(1)
+        a.add(2)
+        a.add(3)
+        a.add(4)
+        a.add(5)
+        a = remove(a,3)
+        a = remove(a,5)
+        a = remove(a,2)
+        a = remove(a,4)
+        a = remove(a,1)
+        self.assertEquals(a, None)
+
+if __name__ == '__main__':
+    unittest.main()
