65d2955f1c9be99313ff31c873868704466a930e
Howard T. Chiam <hchiam@users.noreply.github.com>
2019-10-10 22:19:08 -0400

    Create pack-unpack-operators-asterisk-stars.py

---
+++ pack-unpack-operators-asterisk-stars.py
@@ -0,0 +1,38 @@
+"""
+Demonstrating the powers of the prefix operators * and **
+to pack and unpack iterables/arguments.
+
+Reference: https://treyhunner.com/2018/10/asterisks-in-python-what-they-are-and-how-to-use-them/#Asterisks_in_list_literals
+"""
+
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("Please use Python 3 or later.")
+
+
+fruits = ['apple', 'banana', 'coconut', 'durian']
+numbers = [1, 2, 3, 4, 5]
+print(numbers, fruits, '\n') # [1, 2, 3, ...] ['apple', 'banana', ...]
+print(*numbers, *fruits, '\n') # 1, 2, 3, ..., 'apple', 'banana', ...
+print([*numbers, *fruits], '\n') # [1, 2, 3, ..., 'apple', 'banana', ...]
+
+
+def sillyPrint(*items): # "packs" parameters; variable number of arguments!
+    for item in items:
+        print(item)
+
+sillyPrint('hi', '1', 'hello', '\n')
+# hi
+# 1
+# hello
+
+
+def tag(tag_name, **attributes): # attributes unpacks values from keys
+    attribute_list = [
+        f'{name}="{value}"'
+        for name, value in attributes.items()
+    ]
+    return f"<{tag_name} {' '.join(attribute_list)}>"
+
+print(tag('a', href="https://www.google.com"), '\n') # '<a href="https://www.google.com">'
+print(tag('img', height=20, width=40, src="face.jpg"), '\n') # '<img height="20" width="40" src="face.jpg">'
