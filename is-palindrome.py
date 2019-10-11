66872a81411cb38568a7c327d8df7de3989337f6
Howard T. Chiam <hchiam@users.noreply.github.com>
2019-10-10 22:45:01 -0400

    Update is-palindrome.py

--- is-palindrome.py
+++ is-palindrome.py
@@ -1,7 +1,14 @@
 def is_palindrome(sequence):
-  # zip() and reversed() don't copy the list, and instead return lazy iterators
+  # zip() and reversed() don't copy the list,
+  # and instead return lazy iterators
   # so this does not take extra memory
-  for n, m in zip(sequence, reversed(sequence)):
-    if n != m:
-      return False
-  return True
+  
+  # all() checks if all values are true in the generator
+  # alternatively, we could use "not any(n != m ..." (albeit harder to read)
+  return all(
+    n == m
+    for n, m in zip(sequence, reversed(sequence))
+  )
+
+is_palindrome('1a12') # False
+is_palindrome('1a1') # True
