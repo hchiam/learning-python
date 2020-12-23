1175303a9bcaab806f66b693b39ce976fbd54d9d
hchiam <hchiam@users.noreply.github.com>
2020-12-23 16:57:44 -0500

    update

--- tcp/server.py
+++ tcp/server.py
@@ -8,7 +8,9 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # socket family: AF_INET = IPv4, just one way of routing data
 # socket type: SOCK_STREAM = TCP, which indicates using a 3-way handshake
 
+# host = '192.168.1.###'
 host = socket.gethostname()
+print(host)
 port = 8000
 address = (host, port)
 server_socket.bind(address)
@@ -20,5 +22,5 @@ while True:
     client_socket, client_address = server_socket.accept()
     print('received connection from ' + str(address))
     message_to_client = 'you connected to the server'
-    server_socket.send(message_to_client)
+    server_socket.send(message_to_client.encode())
     client_socket.close()
