f0c37abf34dfc770bbd0d7cb74f1010b9403f203
hchiam <hchiam@users.noreply.github.com>
2021-10-31 21:59:46 -0400

    copy for mini project

---
+++ pdf-scraping/parse-pdf.py
@@ -0,0 +1,46 @@
+import textract  # pip3 install textract
+
+from os import listdir
+from os.path import isfile, join
+
+
+def main():
+    print('\nFor copy-paste convenience, \ndouble-click and copy:\n')
+    show_PDFs_in_current_directory()
+
+    file_name = file_prompt()
+
+    text = textract.process(file_name)
+    pages = split_into_PDF_pages(text)
+    print(f'\nPages: {len(pages)}\n')
+
+
+def show_PDFs_in_current_directory():
+    pdf_file_names = [f for f in listdir('.') if isfile(
+        join('.', f)) and f.endswith('.pdf')]
+    print('\n'.join(pdf_file_names))
+
+
+def file_prompt():
+    file_name = ''
+    try:
+        # Python 3:
+        file_name = input('\nPDF file name?\n')
+    except:
+        # Python 2:
+        file_name = raw_input('\nPDF file name?\n')
+    return file_name
+
+
+def split_into_PDF_pages(text):
+    new_page_character = r'\x0c'
+    pages = str(text).split(new_page_character)
+    return pages
+
+
+def show_stats(text):
+    pages = split_into_PDF_pages(text)
+    print(f'\nPages: {len(pages)}\n')
+
+
+main()
