8055ee3fc54c7274fc44cd19836165b0d078544f
hchiam <hchiam@users.noreply.github.com>
2021-10-31 20:59:17 -0400

    one that actually worked for me

--- parse-pdf.py
+++ parse-pdf.py
@@ -1,21 +1,26 @@
-# https://automatetheboringstuff.com/chapter13
-# pip3 install PyPDF2
-import PyPDF2
-import re
+# pip3 install textract
+import textract
+text = textract.process('...pdf')
+print(text)
 
-pdf_path = '...pdf'
-pdf_file = open(pdf_path, 'rb')
-read_pdf = PyPDF2.PdfFileReader(pdf_file)
-number_of_pages = read_pdf.getNumPages()
-page = read_pdf.getPage(1)
-page_content = page.extractText() # .encode('utf-8')
-attempted_keys_regex = '(\d ?\) ?To .+\n.+\n.+)'
-attempted_MV_regex = '(".+?[#|"]?.+?\(.+?\))' # use with re.DOTALL
-print(re.findall(attempted_keys_regex, page_content))
-print('\n\n\n'.join(re.findall(attempted_MV_regex, page_content, re.DOTALL)))
+# # https://automatetheboringstuff.com/chapter13
+# # pip3 install PyPDF2
+# import PyPDF2
+# import re
 
-# for page_number in range(number_of_pages):
-  # page = read_pdf.getPage(page_number)
-  # page_content = page.extractText().encode('utf-8')
-  # ResSearch = re.search('verse', page_content)
-  # print(ResSearch)
+# pdf_path = '...pdf'
+# pdf_file = open(pdf_path, 'rb')
+# read_pdf = PyPDF2.PdfFileReader(pdf_file)
+# number_of_pages = read_pdf.getNumPages()
+# page = read_pdf.getPage(1)
+# page_content = page.extractText() # .encode('utf-8')
+# attempted_keys_regex = '(\d ?\) ?To .+\n.+\n.+)'
+# attempted_MV_regex = '(".+?[#|"]?.+?\(.+?\))' # use with re.DOTALL
+# print(re.findall(attempted_keys_regex, page_content))
+# print('\n\n\n'.join(re.findall(attempted_MV_regex, page_content, re.DOTALL)))
+
+# # for page_number in range(number_of_pages):
+#   # page = read_pdf.getPage(page_number)
+#   # page_content = page.extractText().encode('utf-8')
+#   # ResSearch = re.search('verse', page_content)
+#   # print(ResSearch)
