e3bb69c99aa9f49f5c1b1c5311ff26b40fc1631f
hchiam <hchiam@users.noreply.github.com>
2021-10-31 22:08:45 -0400

    write and read helper functions

--- write_file.py
+++ write_file.py
@@ -7,6 +7,17 @@
 
 # (NOTE: you can only use one at a time!)
 
+def write(file_name, text):
+    f = open(file_name, "w")
+    f.write(text)
+    f.close()
+
+
+def read(file_name):
+    f = open(file_name, "r")
+    print(f.read())
+
+
 if __name__ == '__main__':
     f = open("write_file_demo.txt", "w")
     f.write("Some content (replaces any existing).")
