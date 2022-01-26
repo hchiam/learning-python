7e3ab138da76fe7e9295d811823a98baa4a6bf7f
hchiam <hchiam@users.noreply.github.com>
2022-01-25 20:33:52 -0500

    example() and autoformat

--- pca.py
+++ pca.py
@@ -7,12 +7,19 @@
 import numpy as np
 from sklearn.decomposition import PCA
 
-data_points = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-pca = PCA(n_components=2)
-pca.fit(data_points)
 
-print(pca.explained_variance_ratio_)
-# [0.99244289 0.00755711]
+def example():
+    data_points = np.array(
+        [[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+    pca = PCA(n_components=2)
+    pca.fit(data_points)
 
-print(pca.singular_values_)
-# [6.30061232 0.54980396]
+    print(pca.explained_variance_ratio_)
+    # [0.99244289 0.00755711]
+
+    print(pca.singular_values_)
+    # [6.30061232 0.54980396]
+
+
+if __name__ == '__main__':
+    example()
