86306aecc6508756a50829ac2a62fb742d60033a
hchiam <hchiam@users.noreply.github.com>
2022-01-25 20:26:02 -0500

    plot() and plot_matrix()

--- plot_graph.py
+++ plot_graph.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
 
 
+def plot(x, y):
+    plt.plot(x, y)
+    plt.show()
+
+
+def plot_matrix(matrix):
+    plt.plot(matrix)
+    plt.show()
+
+
 def example():
     x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     y = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256]
-
-    plt.plot(x, y)
-    plt.show()
+    plot(x, y)
 
 
 if __name__ == '__main__':
