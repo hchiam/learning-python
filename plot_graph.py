80568c526780ad31705c6ffd3214dd1b345ec290
hchiam <hchiam@users.noreply.github.com>
2022-01-25 20:20:59 -0500

    example and autoformat

--- plot_graph.py
+++ plot_graph.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 
-x = [0,1,2,3,4,5,6,7,8,9]
-y = [0,1,2,4,8,16,32,64,128,256]
 
-plt.plot(x, y)
-plt.show()
+def example():
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    y = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256]
+
+    plt.plot(x, y)
+    plt.show()
+
+
+if __name__ == '__main__':
+    example()
