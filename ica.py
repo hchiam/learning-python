8a0575c53e7a0e83d80868d8b0dea76c4d069a85
hchiam <hchiam@users.noreply.github.com>
2022-01-25 20:26:32 -0500

    working PCA and ICA examples

--- ica.py
+++ ica.py
@@ -1,1 +1,26 @@
 # https://towardsdatascience.com/independent-component-analysis-ica-in-python-a0ef0db0955e
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.FastICA.html
+
+# python ica.py
+
+from sklearn.datasets import load_digits
+from sklearn.decomposition import FastICA
+import matplotlib.pyplot as plt
+from plot_graph import plot_matrix
+
+
+def example():
+    X, _ = load_digits(return_X_y=True)
+    transformer = FastICA(n_components=7,
+                          random_state=0,
+                          tol=0.1)
+    X_transformed = transformer.fit_transform(X)
+    print(X_transformed.shape)
+    # (1797, 7)
+
+    plot_matrix(X_transformed)
+
+
+if __name__ == '__main__':
+    example()
--- pca.py
+++ pca.py
@@ -1,1 +1,18 @@
 # https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
+
+# python pca.py
+
+import numpy as np
+from sklearn.decomposition import PCA
+
+data_points = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+pca = PCA(n_components=2)
+pca.fit(data_points)
+
+print(pca.explained_variance_ratio_)
+# [0.99244289 0.00755711]
+
+print(pca.singular_values_)
+# [6.30061232 0.54980396]
